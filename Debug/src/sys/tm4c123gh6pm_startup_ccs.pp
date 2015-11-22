2f7292e822ab0e07bccdd7f00f5b161ab8b3b413
monkaco <monkaco@yahoo.com.br>
2015-11-22 13:35:02 -0200

    Duplicated files deleted

--- Debug/src/sys/tm4c123gh6pm_startup_ccs.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-src/sys/tm4c123gh6pm_startup_ccs.obj: ../src/sys/tm4c123gh6pm_startup_ccs.c
-src/sys/tm4c123gh6pm_startup_ccs.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
-src/sys/tm4c123gh6pm_startup_ccs.obj: D:/Dropbox/myShared/Engenharia/Codigos/Tiva-C/PSO/inc/usr/pso_isr.h
-
-../src/sys/tm4c123gh6pm_startup_ccs.c: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
-D:/Dropbox/myShared/Engenharia/Codigos/Tiva-C/PSO/inc/usr/pso_isr.h: 
--- Debug/src/usr/ulink_bridge.pp
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-src/usr/ulink_bridge.obj: ../src/usr/ulink_bridge.c
-
-../src/usr/ulink_bridge.c: 
